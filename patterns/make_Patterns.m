90c66c45559321ebb6be5ae0c13334e2a3889d27
Yvette <evettita@gmail.com>
2017-08-02 19:37:56 -0400

    Switching PATTERNS and Positionfunctions to new v1.3  era!!!

--- patterns/make_Patterns.m
+++ patterns/make_Patterns.m
@@ -1,4 +1,74 @@
-%make_Patterns_YEF
+%make_Patterns
+
+%% LIGHT ON dots at each location on the screen
+%   The dot location is encoded as combinations of x dim and y dim
+%   xpos = 56 and y pos = 16 are the full screen dark. 
+
+clear all;
+numOfPanelsAcross = 7;% 7 panels across
+numOfPanelsVertically = 2;%
+LEDdotsPerPanel = 8; % this shouldn't change!  LEDs are always 8 dots in x and y. 
+
+DOT_WIDTH = 2; % number of LED dots wide
+
+LEDdotsAcross = numOfPanelsAcross * LEDdotsPerPanel; % 56 for yvette's set up
+LEDdotsVertically = numOfPanelsVertically * LEDdotsPerPanel;% 16 for yvette's current set up
+
+%Save general infomation about pattern layout
+pattern.x_num = LEDdotsAcross; % this variable will where the dot is on the screen in x, last dim = 56 is blank
+pattern.y_num = LEDdotsVertically; 		% Y will store where the dot is on the screen in y, last dim = 16 is blank
+
+pattern.num_panels = 14; 	% This is the number of unique Panel IDs required.
+pattern.gs_val = 2; 	% This pattern gray scale value
+
+Pats = zeros(LEDdotsVertically, LEDdotsAcross, pattern.x_num, pattern.y_num); 	%initializes the array with zeros
+
+% Construct the dot patterns within each dimention
+% zeros 0 = dark, ones 1 = light
+
+for ypos = 1: LEDdotsVertically - ( DOT_WIDTH - 1)   
+    % build intial dot pattern for this y position
+    dot_pattern = zeros( LEDdotsVertically , LEDdotsAcross) ;
+    dot_pattern( ypos: ypos + DOT_WIDTH - 1 , 1 : DOT_WIDTH ) = 1; % draw light dot into matrix
+    
+
+    for xpos = 1: LEDdotsAcross - ( DOT_WIDTH - 1)
+        
+        % shift dot_pattern to each different location depending on current
+        % x pos
+        Pats(:, :, xpos , ypos) = ShiftMatrix (dot_pattern, (xpos - 1),'r','y'); % place 
+       
+    end
+end
+
+% Make sure whole matrix is blank when x or y is max  = 0 for a blank
+% screen,
+Pats(:, : , pattern.x_num, pattern.y_num) = 0;
+
+pattern.Pats = Pats; 		% put data in structure 
+
+pattern.Panel_map = [4, 5, 12, 14, 6, 11, 13 ; 1 , 2 ,7, 10, 3, 8, 9]; 	% define panel structure vector - YEF arena updated 8/2017
+
+pattern.BitMapIndex = process_panel_map(pattern);
+pattern.data = Make_pattern_vector(pattern);
+ 
+ %% SAVE pattern place to save patterns to be put on the SD card:
+% place to save patterns to be put on the SD card:
+ directory_name = 'C:\Users\Wilson\Documents\GitHub\panels-patternsAndFunctions_YEF\patterns';
+ str = [directory_name '\Pattern_001_2pixelBrightDot']; 	% name must begin with ‘Pattern_’
+ save(str, 'pattern');
+ 
+%%
+
+
+
+
+
+
+
+
+
+%% OLD CODE for reference right now while writing:
 
 
 %% Vertical Gratting Pattern  4 pixels wide
