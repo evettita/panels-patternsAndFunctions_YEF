b5e767fac0ccede8291ba98f9b51de342f0a34a3
Yvette <evettita@gmail.com>
2018-02-05 17:27:00 -0500

    added moving grating stimuli for optomotor experiments

--- functions/make_positionFunction.m
+++ functions/make_positionFunction.m
@@ -521,3 +521,112 @@ func = positionArray;
  directory_name = 'C:\Users\Wilson\Documents\GitHub\panels-patternsAndFunctions_YEF\functions\';
  str_x = [directory_name '\position_function_013_movingLeftPattern_15degS']; 
  save(str_x, 'func'); % variable must be named 'func'
+ 
+
+ %%  Interleaved moving bar stimulus 15 DEg/s   rightward then leftward
+PANELS_FRAME_RATE = 50; %Hz
+POSITION_FUNCTION_LENGTH = 1000; % this how many frames long these normally are... set by panels
+numOfPanelsAcross = 12;% 7 panels across
+numOfPanelsVertically = 2;%
+LEDdotsPerPanel = 8; % this shouldn't change!  LEDs are always 8 dots in x and y. 
+
+LEDdotsAcross = numOfPanelsAcross * LEDdotsPerPanel; % 96 for yvette's set up including fictive panels
+
+PATTERN_SPEED_DEG_PER_SEC = 15;% deg/s  
+DEGREES_PRE_PIXEL = 3; % deg %CHECK!  THIS IS APPROXIMATE!!
+
+% Warning zero indexing for the position funciton!!
+XDimMin = 0;% 
+XDimMax = LEDdotsAcross;% 
+
+rightwardPositionArray = [];
+leftwardPositionArray = [];
+
+pixelPerSecond = PATTERN_SPEED_DEG_PER_SEC / DEGREES_PRE_PIXEL; % corresponding LED pixel per second
+%how many frames should be spent at each function position.
+framesDwellPerPixel =  PANELS_FRAME_RATE / pixelPerSecond; % frames/pixel=( (frames/s) / (pixel/s) )
+
+%Build Rightward segement of array:
+currFrameCounter = XDimMin;
+while (length (rightwardPositionArray) < POSITION_FUNCTION_LENGTH) && (currFrameCounter <= XDimMax)
+    
+    addToArray = currFrameCounter * ones( 1,  framesDwellPerPixel );
+
+    rightwardPositionArray = [rightwardPositionArray addToArray ];
+    
+    currFrameCounter = currFrameCounter + 1;     
+end
+
+%Build leftward segement of array:
+currFrameCounter = XDimMax - 1;
+while (length (leftwardPositionArray) < POSITION_FUNCTION_LENGTH) && (currFrameCounter > XDimMin)
+    
+    addToArray = currFrameCounter * ones( 1,  framesDwellPerPixel );
+
+    leftwardPositionArray = [leftwardPositionArray addToArray ];
+    
+    currFrameCounter = currFrameCounter - 1;  
+end
+
+NUM_TIMES_TO_REPEAT_GRATING_ROTAION = 5;
+func = [ repmat( rightwardPositionArray , 1, NUM_TIMES_TO_REPEAT_GRATING_ROTAION)  repmat( leftwardPositionArray , 1, NUM_TIMES_TO_REPEAT_GRATING_ROTAION) ];
+
+%% SAVE position function place to be put on the SD card:
+% place to save patterns to be put on the SD card:
+ directory_name = 'C:\Users\Wilson\Documents\GitHub\panels-patternsAndFunctions_YEF\functions\';
+ str_x = [directory_name '\position_function_014_movingGratingRightLeft_15degS']; 
+ save(str_x, 'func'); % variable must be named 'func'
+
+ %%  Interleaved moving bar stimulus 50 deg/s
+PANELS_FRAME_RATE = 50; %Hz
+POSITION_FUNCTION_LENGTH = 1000; % this how many frames long these normally are... set by panels
+numOfPanelsAcross = 12;% 7 panels across
+numOfPanelsVertically = 2;%
+LEDdotsPerPanel = 8; % this shouldn't change!  LEDs are always 8 dots in x and y. 
+
+LEDdotsAcross = numOfPanelsAcross * LEDdotsPerPanel; % 96 for yvette's set up including fictive panels
+
+PATTERN_SPEED_DEG_PER_SEC = 50;% deg/s  
+DEGREES_PRE_PIXEL = 3; % deg %CHECK!  THIS IS APPROXIMATE!!
+
+% Warning zero indexing for the position funciton!!
+XDimMin = 0;% 
+XDimMax = LEDdotsAcross;% 
+
+rightwardPositionArray = [];
+leftwardPositionArray = [];
+
+pixelPerSecond = PATTERN_SPEED_DEG_PER_SEC / DEGREES_PRE_PIXEL; % corresponding LED pixel per second
+%how many frames should be spent at each function position.
+framesDwellPerPixel =  PANELS_FRAME_RATE / pixelPerSecond; % frames/pixel=( (frames/s) / (pixel/s) )
+
+%Build Rightward segement of array:
+currFrameCounter = XDimMin;
+while (length (rightwardPositionArray) < POSITION_FUNCTION_LENGTH) && (currFrameCounter <= XDimMax)
+    
+    addToArray = currFrameCounter * ones( 1,  framesDwellPerPixel );
+
+    rightwardPositionArray = [rightwardPositionArray addToArray ];
+    
+    currFrameCounter = currFrameCounter + 1;     
+end
+
+%Build leftward segement of array:
+currFrameCounter = XDimMax - 1;
+while (length (leftwardPositionArray) < POSITION_FUNCTION_LENGTH) && (currFrameCounter > XDimMin)
+    
+    addToArray = currFrameCounter * ones( 1,  framesDwellPerPixel );
+
+    leftwardPositionArray = [leftwardPositionArray addToArray ];
+    
+    currFrameCounter = currFrameCounter - 1;  
+end
+
+NUM_TIMES_TO_REPEAT_GRATING_ROTAION = 5;
+func = [ repmat( rightwardPositionArray , 1, NUM_TIMES_TO_REPEAT_GRATING_ROTAION)  repmat( leftwardPositionArray , 1, NUM_TIMES_TO_REPEAT_GRATING_ROTAION) ];
+
+%% SAVE position function place to be put on the SD card:
+% place to save patterns to be put on the SD card:
+ directory_name = 'C:\Users\Wilson\Documents\GitHub\panels-patternsAndFunctions_YEF\functions\';
+ str_x = [directory_name '\position_function_015_movingGratingRightLeft_50degS']; 
+ save(str_x, 'func'); % variable must be named 'func'
--- patterns/make_Patterns.m
+++ patterns/make_Patterns.m
@@ -654,7 +654,63 @@ pattern.data = Make_pattern_vector(pattern);
  directory_name = 'C:\Users\Wilson\Documents\GitHub\panels-patternsAndFunctions_YEF\patterns';
  str = [directory_name '\Pattern_011_2_2pixelBars_60Deg']; 	% name must begin with ‘Pattern_’
  save(str, 'pattern');
- %%
+%% 4 pixel wide grating, 8 pixel period:
+clear all;
+numOfPanelsAcross = 12;% 9;% 7 panels across
+numOfPanelsVertically = 2;%
+LEDdotsPerPanel = 8; % this shouldn't change!  LEDs are always 8 dots in x and y. 
+
+BAR_WIDTH = 4; % number of LED dots wide
+
+LEDdotsAcross = numOfPanelsAcross * LEDdotsPerPanel; % 56 for yvette's set up
+LEDdotsVertically = numOfPanelsVertically * LEDdotsPerPanel;% 16 for yvette's current set up
+
+%Save general infomation about pattern layout
+pattern.x_num = LEDdotsAcross; % this variable will where the dot is on the screen in x, last dim = 56 is blank
+pattern.y_num = 2; 		% Y will encode if the bar is displayed=1, not displayed= 2;
+
+pattern.num_panels = 24; %18; 	% This is the number of unique Panel IDs required.
+pattern.gs_val = 2; 	% This pattern gray scale value
+
+Pats = zeros(LEDdotsVertically, LEDdotsAcross, pattern.x_num, pattern.y_num); 	%initializes the array with zeros
+
+% Construct the grating pattern in Y = 2, and all x values
+% zeros 0 = dark, ones 1 = light
+PATTERN_INDEX = 1;
+gratingWidth = 4;
+grating = zeros( 1 , LEDdotsAcross);
+
+for i = 1: LEDdotsAcross  
+    if( mod( i , gratingWidth * 2) < ( gratingWidth ) )
+        grating(i) = 1; % add in light stripes at each of these locations
+    end
+end
+
+stripe_pattern = repmat ( grating, LEDdotsVertically,  1); % extend matrix to include the whole vertical extent of the panels
+
+Pats(:, :, 1, PATTERN_INDEX) = stripe_pattern;
+
+for j = 2:LEDdotsAcross 			%use ShiftMatrixPats to rotate stripe image
+    Pats(:,:,j, PATTERN_INDEX) = ShiftMatrix(Pats(:,:,j-1,PATTERN_INDEX),1,'r','y');
+end
+
+
+% Make sure whole matrix is blank when x or y is max  = 0 for a blank
+% screen,
+Pats(:, : , pattern.x_num, pattern.y_num) = 0;
+
+pattern.Pats = Pats; 		% put data in structure 
+pattern.Panel_map = [9, 12, 13, 15, 17, 14, 16, 18, 8 , 19, 20, 21 ; 1 , 5 ,2, 6, 10, 3, 7, 11, 4, 22, 23, 24 ]; 	% 360 degree arena updated 10/25/17 define panel structure vector
+% panels 19-24 are fictive and do not actually exist on hardware!
+
+pattern.BitMapIndex = process_panel_map(pattern);
+pattern.data = Make_pattern_vector(pattern);
  
+%% SAVE pattern place to save patterns to be put on the SD card:
+% place to save patterns to be put on the SD card:
+ directory_name = 'C:\Users\Wilson\Documents\GitHub\panels-patternsAndFunctions_YEF\patterns';
+ str = [directory_name '\Pattern_012_4pixelGrating']; 	% name must begin with ‘Pattern_’
+ save(str, 'pattern');
  
+%%
  
